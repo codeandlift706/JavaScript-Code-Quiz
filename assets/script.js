6b4dd58fafe0f551003fdc1664af0e6851e9f922
Code Queen <channguyen97@gmail.com>
2023-08-28 16:06:34 -0700

    added section elements

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+//global variables
+const hidePage = document.querySelector("")
+const container = document.querySelector("")
+
+const mode ="full-display";
+
+//functions -- how we understand and define the actions we want to take
+
+
+//processes -- how it will execute
+//EventListener
