dd3dfb6c20f53b9abf0e5ba6c6d910ac57232a34
Code Queen <channguyen97@gmail.com>
2023-08-28 21:04:10 -0700

    added the function to hide the <header> element when you click the start button

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,44 @@
 //global variables
-const hidePage = document.querySelector("")
-const container = document.querySelector("")
+//variables to dynamically change classes
+//const pageSwitcher = document.querySelector("#pageSwitcher");
+//const container = document.querySelector("#frontPage");
 
-const mode ="full-display";
+//default mode is to only show the starting page
+// const mode ="introPageOnly";
 
 //functions -- how we understand and define the actions we want to take
 
 
 //processes -- how it will execute
-//EventListener
+//pageSwitcher.addEventListener("click", function() { //process to dynamically change classes, to "hide"
+//   if (mode === "introPageOnly") {
+//  mode = "quizPage";
+//  container.setAttribute("class", "quizPage");
+//  }
+
+//  else {
+//   mode = "introPageOnly";
+//  container.setAttribute("class", "introPageOnly");
+//   }
+// });
+
+function defaultHomepage() {
+    var sectionEl = document.querySelectorAll("section");
+    if (sectionEl.style.display == "none") {
+        sectionEl.style.display = "block";
+    } else {
+        sectionEl.style.display = "none";
+    }
+}
+
+function pageSwitcherFunction() {
+    var x = document.querySelector(".frontPage");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+    }
+
+defaultHomepage();
+
